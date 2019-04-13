9735d4101ae82e109214c8582bedfb231631a137
TheRealJoeFriel <jfriel46@gmail.com>
2019-04-13 01:04:42 -0400

    added callback

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -2,15 +2,35 @@ var ClientID = '154106d34b2d4c9b8a8a1c55561771ce'
 var RedirectURI = 'http://therealjoefriel.github.io/Bitcamp2019/callback.html'
 var responseType = 'token';
 
+function getURL() {
+    'https://accounts.spotify.com/authorize?client_id='+ClientID+
+        '&redirect_uri=' + RedirectURI + 
+        '&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
+        '&response_type=' + responseType;
+}
+
 function connect() {
-    var url = 'https://accounts.spotify.com/authorize?client_id='+ClientID+
-    '&redirect_uri=' + RedirectURI + 
-    '&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
-    '&response_type=' + responseType;
+    return {
+        login: function() {
+            var url = getURL();
 
-    var width = 400, height = 600, left = (screen.width / 2) - (width / 2), top = (screen.height / 2) - (height / 2);
+            var width = 400, height = 600, left = (screen.width / 2) - (width / 2), top = (screen.height / 2) - (height / 2);
 
-    return loginWindow = window.open(url, 'Spotify', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
+            return loginWindow = window.open(url, 'Spotify', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
+        
+        },
+
+        getAccessToken: function() {
+            var expires = 0 + localStorage.getItem('pa_expires', '0');
+				if ((new Date()).getTime() > expires) {
+					return '';
+				}
+            var token = localStorage.getItem('pa_token', '');
+            return token;
+        },
+        
+    }
+
 }
 
 function search(searchQuery) {
