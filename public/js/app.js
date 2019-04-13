b86306b372dc2c7c1b5b23d48b0bbdce0d852149
TheRealJoeFriel <jfriel46@gmail.com>
2019-04-13 14:42:14 -0400

    set up express

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+var hostname = "localhost";
+var port = "8080";
+
 // first parameter is the mount point, second is the location in the file system
-app.use("/public", express.static(__dirname));
+app.use(express.static(__dirname + "/../"));
 
 app.get('/', function(req, res) {
-    res.redirect('../index.html');
+    res.redirect('index.html');
 });
 
-app.listen(8080);
+app.listen(port);
+console.log(`Running on ${hostname}:${port}`);
