f127a9d2931388883fbbdd2b8f9e9ea2e2b88368
TheRealJoeFriel <jfriel46@gmail.com>
2019-04-12 23:46:41 -0400

    maybe established connection

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -1,12 +1,16 @@
 var ClientID = '154106d34b2d4c9b8a8a1c55561771ce'
+var RedirectURI = 'http://spotify.com'
 var responseType = 'token';
 
 function connect() {
-    var theUrl = "https://accounts.spotify.com/authorize?client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123";
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, false); 
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+    var url = 'https://accounts.spotify.com/authorize?client_id='+ClientID+
+    '&redirect_uri=' + RedirectURI + 
+    '&scope=playlist-read-private%20playlist-modify%20playlist-modify-private' +
+    '&response_type=' + responseType;
+
+    var width = 400, height = 600, left = (screen.width / 2) - (width / 2), top = (screen.height / 2) - (height / 2);
+
+    return loginWindow = window.open(url, 'Spotify', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left);
 }
 
 function search(searchQuery) {
