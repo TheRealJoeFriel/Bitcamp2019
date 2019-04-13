1c0787d5514cb282d0c6256e04b6a728f3965816
TheRealJoeFriel <jfriel46@gmail.com>
2019-04-12 23:00:12 -0400

    spotify connection

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -1,7 +1,20 @@
 var ClientID = '154106d34b2d4c9b8a8a1c55561771ce'
+var responseType = 'token';
 
 function connect() {
-    alert("hello world")
+    var theUrl = "https://accounts.spotify.com/authorize?client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); 
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function search(searchQuery) {
+    var searchResult = "https://api.spotify.com/v1/${searchQuery}";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); 
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
 }
 
 function loadPlaylist() {
